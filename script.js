fbb6bd61de7705ecc556d8bb7374ac1881df378f
martin <martin@siarp.de>
2012-08-24 16:23:15 +0200

    hello world innerHTML

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 var buttonNode = document.getElementById('send');
 buttonNode.addEventListener('click', function(event) {
-	var displayNode = document.getElementById('display');
-	var text = 'Hello World!';
-	var textNode = document.createTextNode(text);
-	displayNode.appendChild(textNode);
+	document.getElementById('display').innerHTML = 'Hello World!';
 });
