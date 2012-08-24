979c397f0e460ab5fad53e4b9682e53aff07a01b
martin <martin@siarp.de>
2012-08-24 17:13:39 +0200

    add timer that calls ajax

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 var buttonNode = document.getElementById('send');
 buttonNode.addEventListener('click', function(event) {
 	ajax();
+	startTimer(ajax);
 });
 
 function ajax () {
 	var xhr = new XMLHttpRequest();
-	xhr.open('GET', 'http://80.157.2.254/firefox%20OS/hallo.json');
+	xhr.open('GET', 'http://80.157.2.254/firefox%20OS/random.php');
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState == 4) {
 			if (xhr.status == 200) {
@@ -16,4 +17,8 @@ function ajax () {
 		}
 	};
 	xhr.send();
+}
+
+function startTimer (callback) {
+	setInterval(callback, 1000);
 }
