0336623dbc109fe67b0178cc80457442a727fd98
martin <martin@siarp.de>
2012-08-24 16:55:06 +0200

    ajax on click

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 var buttonNode = document.getElementById('send');
 buttonNode.addEventListener('click', function(event) {
-	document.getElementById('display').innerHTML = 'Hello World!';
+	ajax();
 });
 
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://80.157.2.254/firefox%20OS/hallo.json');
-xhr.onreadystatechange = function () {
-	if (xhr.readyState == 4) {
-		if (xhr.status == 200) {
-	        alert(xhr.responseText);
-	    } else {
-	        alert('There was a problem with the request.');
-	    }
-	}
-};
-xhr.send();
+function ajax () {
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'http://80.157.2.254/firefox%20OS/hallo.json');
+	xhr.onreadystatechange = function () {
+		if (xhr.readyState == 4) {
+			if (xhr.status == 200) {
+		        document.getElementById('display').innerHTML = xhr.responseText;
+		    } else {
+		        alert('There was a problem with the request.');
+		    }
+		}
+	};
+	xhr.send();
+}
